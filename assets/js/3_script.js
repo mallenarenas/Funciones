79778eb4ef4344d8f9d9b9cd7d9a7fcc402a7bd4
Mallen Arenas <mallenarenasc@gmail.com>
2022-07-15 18:36:39 -0400

    changes in 3_script.js

--- assets/js/3_script.js
+++ assets/js/3_script.js
@@ -6,22 +6,22 @@
 // ele.addEventListener("click", pintar);
 
 // 1- Primera modificación: (elemento clickeado como argumento)
-function pintar(element){
-      element.style.backgroundColor = 'yellow'
-}
- let ele = document.getElementById("ele1")
- ele.addEventListener("click",() => { 
-      pintar(ele)
-});
+// function pintar(element){
+//       element.style.backgroundColor = 'yellow'
+// }
+//  let ele = document.getElementById("ele1")
+//  ele.addEventListener("click",() => { 
+//       pintar(ele)
+// });
       
 // 2- Segunda modificación: (función con argumento color)
-// function pintar( element , color='green'){
-//       element.style.backgroundColor = color
-//     }
-// let ele = document.getElementById("ele1")
-// ele.addEventListener("click", () => {
-//       pintar(ele, 'yellow')
-// });
+function pintar( element , color='green'){
+      element.style.backgroundColor = color
+    }
+let ele = document.getElementById("ele1")
+ele.addEventListener("click", () => {
+      pintar(ele, 'yellow')
+});
 
 
 
